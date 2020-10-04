582b7c01a1c02888cc3df7788018e21f000373d3
fadilxcoder <rosunmungurudm@gmail.com>
2020-10-04 10:59:24 +0000

    MongoDB manipulation & GUI

--- README.md
+++ README.md
@@ -1,28 +1,35 @@
-# Docker, MongoDB & PHP on AWS C9
+# Docker, MongoDB (GUI) & PHP on AWS C9
 
 - All configs regarding building the docker container is in `docker` folder
 - 2 files needed for mongodb : `Docker` & `mongod.conf` - **Important settings in mongod.conf (bindIp, dbPath, path)**
 - 2 folders needed for mongodb : `db` & `logs`
+ 
+## Resources
+
+- mongo-gui (Viewing MongoDB) - https://www.npmjs.com/package/mongo-gui
 
 ## SetUp
 
 - Delete all images : `docker rmi -f $(docker images -a -q)` - *optional*
 - Delete containers : `docker rm fx_mongo_database fx_nginx fx_php_fpm` **names found in `docker-compose.yml`, use `docker ps -a` to check if name of containers exist** - *optional*
-- Chanage folders rights : `chmod 777 docker/mongodb/db/ docker/mongodb/logs/``
+- Change folders rights : `chmod 777 docker/mongodb/db/ docker/mongodb/logs/`
 - Build application : `docker-compose up --build`
  
 ## App settings
 
-- In php file, connecting to mongoDB : `(new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;`, *where most settings is in docker-compose.yml file & IP is from **fx_nginx** *
+- In php file, connecting to mongoDB : `(new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;`, *where most settings is in docker-compose.yml file & IP is from **fx_nginx***
 
  
 ## Go live
 
 - Install NGROK
 - use `docker ps -a` to see PORTS
-- Start NGROK : `ngrok http 8881`
 
-## Start
+## Start application
 
 - Start docker by `docker-compose up`
-- Start NGROK by `ngrok http 8881` *where 8881 is port*
+- Start NGROK (App) by `ngrok http 8881` *where 8881 is port*
+
+## Start Mongo GUI
+
+- After containers are UP, Start NGROK (Mongo GUI) by `nngrok http 4321` *where 4321 is port*
--- src/index.php
+++ src/index.php
@@ -1,34 +1,79 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>MONGO DB & PHP</title>
+        <meta charset="utf-8">
+        <?php # Added in order to prevent NGROK triggering 'GET /favicon.ico' ?>
+        <link rel="icon" href="data:,">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <?php 
 
-    require_once __DIR__ . '/vendor/autoload.php';
-    
-    $collection = (new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;
-    
-    $collection = $collection->users;
+            require_once __DIR__ . '/vendor/autoload.php';
+            
+            $db = (new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;
+            
+            $randArray = [
+                [
+                    'v1' => 1,
+                ],
+                [
+                    'v1' => 1,
+                    'v2' => 2,
+                ],
+                [
+                    'v1' => 1,
+                    'v2' => 2,
+                    'v3' => 3,
+                ],
+                [
+                    'v1' => 1,
+                    'v2' => 2,
+                    'v3' => 3,
+                    'v4' => 4,
+                ],
+                [
+                    'v1' => 1,
+                    'v2' => 2,
+                    'v3' => 3,
+                    'v4' => 4,
+                    'v5' => 5,
+                ],
+                [
+                    'v1' => 1,
+                    'v2' => 2,
+                    'v3' => 3,
+                    'v4' => 4,
+                    'v5' => 5,
+                    'v6' => 6,
+                ],
+                [
+                    'v1' => 1,
+                    'v2' => 2,
+                    'v3' => 3,
+                    'v4' => 4,
+                    'v5' => 5,
+                    'v6' => 6,
+                    'v7' => 7,
+                ],
+            ];
+            
+            $insertArr = [
+                'username' => 'FX-' . rand(1, 999),
+                'email' => 'fadil@xcoder.dvlpr',
+                'date_created' => date('Y/m/d H:i:s'),
+            ];
 
-    $insertManyResult = $collection->insertMany([
-        [
-            'username' => 'admin',
-            'email' => 'admin@example.com',
-            'name' => 'Admin User',
-        ],
-        [
-            'username' => 'test',
-            'email' => 'test@example.com',
-            'name' => 'Test User',
-        ],
-    ]);
-    
-    printf("Inserted %d document(s)\n", $insertManyResult->getInsertedCount());
-    
-    var_dump($insertManyResult->getInsertedIds());
-    echo '<hr>';
-    var_dump($collection);
-    
-    $document = $collection->findOne(['username' => 'admin']);
-    echo '<hr>';
-    var_dump($document);
-
-    
-?>
+            
+            $combineArray = $insertArr + $randArray[rand(1,6)];
+            
+            $usersCollection = $db->users;
+            
+            $insertOneResult = $usersCollection->insertOne([$combineArray]);
+        
+            var_dump($insertOneResult->getInsertedId());
+        ?>
+    </body>
+</html>
 
