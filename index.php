91778af6c030dbae71e1bdd5d8d7f10bd666f868
Fadil <rosunmungurudm@gmail.com>
2020-10-16 00:07:00 +0400

    deploy v13

--- index.php
+++ index.php
@@ -16,8 +16,7 @@
 
             require_once __DIR__ . '/vendor/autoload.php';
             
-            // $db = (new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;
-			$db = (new MongoDB\Client())->mydb;
+            $db = (new MongoDB\Client('mongodb+srv://heroku-usr:XOwbCWtclBzXAK5t@gfx-cluster.hl29j.mongodb.net'))->mydb;
             
             $randArray = [
                 [
