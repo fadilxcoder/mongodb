b1f94dd8307ae453a9cf6d93aa883dac6ca7f00c
Fadil <rosunmungurudm@gmail.com>
2020-10-15 23:13:53 +0400

    deploy v3

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
 
             require_once __DIR__ . '/vendor/autoload.php';
             
-            $db = (new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;
+            // $db = (new MongoDB\Client('mongodb://myuser:docker@172.19.0.1:27017'))->mydb;
+			$db = (new MongoDB\Client())->mydb;
+			var_dump($db->listDatabases());
             
             $randArray = [
                 [
